519c9ead62d5e70308e3f16e7c50702cb6b63e85
rachfuerst <rach.fuerst@gmail.com>
2019-11-16 14:53:18 -0600

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,16 +3,16 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    num_1 = Kernel.rand(1..20)
+    num_1 = Kernel.rand(1..20)# Generate random numbers
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
  
-    resp.write "#{num_1}\n"
+    resp.write "#{num_1}\n" # Write each number to the browser (this is a Rack macro)
     resp.write "#{num_2}\n"
     resp.write "#{num_3}\n"
  
     if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
+      resp.write "You Win" # Again write to the browser
     else
       resp.write "You Lose"
     end
